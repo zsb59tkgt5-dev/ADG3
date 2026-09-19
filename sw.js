7585b3fa9ff8901325d4ed0a1ee9f0a009edb08b
zsb59tkgt5-dev <zsb59tkgt5@privaterelay.appleid.com>
2026-09-18 22:46:12 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE="adg3-v1";
+const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-192.svg","./icon-512.svg"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
